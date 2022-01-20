9b76ddfa79652dfbf8c2ca414fcc9f921e3dad8f
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-20 17:46:58 +0400

    Initial commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Irakli Sokhaneishvili on 20.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
