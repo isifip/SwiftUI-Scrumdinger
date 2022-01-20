5d1d91422ad133fd79749d3edd44bbe6a854d79e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-20 19:06:10 +0400

    + ScrumView, List

--- ScrumDinger/ScrumDingerApp.swift
+++ ScrumDinger/ScrumDingerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumDingerApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingVIew()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
---
+++ ScrumDinger/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  ScrumDinger
+//
+//  Created by Irakli Sokhaneishvili on 20.01.22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
