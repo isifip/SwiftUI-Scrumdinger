294d33cab114d979fa7ed6382e7c59623525ba2a
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-20 19:27:15 +0400

    + Navigation Hierarchy + DetailView

---
+++ ScrumDinger/DetailView.swift
@@ -0,0 +1,56 @@
+//
+//  DetailView.swift
+//  ScrumDinger
+//
+//  Created by Irakli Sokhaneishvili on 20.01.22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section("Meeting Info") {
+                NavigationLink(destination: MeetingVIew()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .accessibilityLabel("Start meeting")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section("Attendees") {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
--- ScrumDinger/Models/Theme.swift
+++ ScrumDinger/Models/Theme.swift
@@ -30,4 +30,8 @@ enum Theme: String {
     var mainColor: Color {
         Color(rawValue)
     }
+    
+    var name: String {
+        rawValue.capitalized
+    }
 }
